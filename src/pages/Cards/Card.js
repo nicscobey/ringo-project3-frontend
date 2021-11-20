03ab76bb63335118a684e982c315d7526f3cf797
Maximilian Rice <maximilian.rice1@gmail.com>
2021-11-20 17:10:27 -0500

    Cards.js in progress

--- src/pages/Cards/Card.js
+++ src/pages/Cards/Card.js
@@ -1,8 +1,44 @@
-import {Switch, Route} from 'react-router-dom'
+import { Switch } from "react-router-dom"
+import { Route } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { useState } from "react"
 
-const Card = () => {
+
+
+const Card = (props) => {
+    
+    const id = props.match.params.id
+    const card = props.card
+    // const cards = card.find(p => p._id === id_)
+    
+    // *DO WE NEED THIS?: state for form
+    // const [editCard, setEditCard] = useState(card) // DO WE NEED THIS?
+
+    // *DO WE NEED THIS?: handleSubmit function for form
+    // const handleSubmit = event => {
+    //     event.preventDefault()
+    //     props.updatePeople(editForm)
+    //     props.history.push("/")
+    // }
+
+    // *DO WE NEED THIS?: delete/remove card from deck
+    // const removeCard = () => {
+    //     props.deleteDeck(card._id)
+    //     props.history.push("/")
+    // }
+
     return (
-        <h1>Card</h1>
+    <div className="card">
+
+        <h1>{card.word}</h1> 
+        <h2>Definition: {card.definition}</h2>
+        <h3>Example: {card.example}</h3>
+        <button id="delete" onClick={deleteCard}>
+            Delete Card
+        </button>
+        
+
+    </div>
     )
 }
 
