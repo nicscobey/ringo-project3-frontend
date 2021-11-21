4fe5b3f54f9c3cb6c95163a93922a49f17eb0419
Nic Scobey <nicscobey@gmail.com>
2021-11-21 14:27:37 -0800

    test color red

---
+++ src/components/ColorTheme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material"
+import { indigo } from "@mui/material/colors"
+
+const ColorTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#f44336'
+        }
+    }
+})
+
+export default ColorTheme
